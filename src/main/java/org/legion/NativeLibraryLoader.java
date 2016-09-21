b89443dd272daef7dda63529bd7b904dc805b21f
Noah Watkins <noahwatkins@gmail.com>
2016-09-21 13:29:55 -0700

    build: update for osx and cmake
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/main/java/org/legion/NativeLibraryLoader.java
+++ src/main/java/org/legion/NativeLibraryLoader.java
@@ -4,15 +4,16 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 
+import org.legion.util.Environment;
+
 public class NativeLibraryLoader {
   //singleton
   private static final NativeLibraryLoader instance = new NativeLibraryLoader();
   private static boolean initialized = false;
 
-  private static final String sharedLibraryName = "liblegion_jni.so";
-  private static final String tempFilePrefix = "liblegion_jni";
-  private static final String tempFileSuffix = ".so";
-
+  private static final String jniLibraryFileName = Environment.getJniLibraryFileName("legion");
+  private static final String tempFilePrefix = "liblegionjni";
+  private static final String tempFileSuffix = Environment.getJniLibraryExtension();
   /**
    * Get a reference to the NativeLibraryLoader
    *
@@ -38,32 +39,44 @@ public class NativeLibraryLoader {
    */
   public synchronized void loadLibraryFromJar(final String tmpDir) throws IOException {
     if (!initialized) {
-      final File temp;
-      if (tmpDir == null || tmpDir.equals("")) {
-        temp = File.createTempFile(tempFilePrefix, tempFileSuffix);
-      } else {
-        temp = new File(tmpDir, sharedLibraryName);
-      }
-
-      if (!temp.exists()) {
-        throw new RuntimeException("File " + temp.getAbsolutePath() + " does not exist.");
-      } else {
-        temp.deleteOnExit();
-      }
-
-      // attempt to copy the library from the Jar file to the temp destination
-      try (final InputStream is = getClass().getClassLoader().
-          getResourceAsStream(sharedLibraryName)) {
-        if (is == null) {
-          throw new RuntimeException(sharedLibraryName + " was not found inside JAR.");
-        } else {
-          Files.copy(is, temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        }
-      }
-
-      System.load(temp.getAbsolutePath());
+      System.load(loadLibraryFromJarToTemp(tmpDir).getAbsolutePath());
       initialized = true;
     }
+  }
+
+  File loadLibraryFromJarToTemp(final String tmpDir) throws IOException {
+    final File temp;
+    if (tmpDir == null || tmpDir.isEmpty()) {
+      temp = File.createTempFile(tempFilePrefix, tempFileSuffix);
+    } else {
+      temp = new File(tmpDir, jniLibraryFileName);
+      if (temp.exists() && !temp.delete()) {
+        throw new RuntimeException("File: " + temp.getAbsolutePath()
+            + " already exists and cannot be removed.");
+      }
+      if (!temp.createNewFile()) {
+        throw new RuntimeException("File: " + temp.getAbsolutePath()
+            + " could not be created.");
+      }
+    }
+
+    if (!temp.exists()) {
+      throw new RuntimeException("File " + temp.getAbsolutePath() + " does not exist.");
+    } else {
+      temp.deleteOnExit();
+    }
+
+    // attempt to copy the library from the Jar file to the temp destination
+    try (final InputStream is = getClass().getClassLoader().
+        getResourceAsStream(jniLibraryFileName)) {
+      if (is == null) {
+        throw new RuntimeException(jniLibraryFileName + " was not found inside JAR.");
+      } else {
+        Files.copy(is, temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
+      }
+    }
+
+    return temp;
   }
 
   /**
